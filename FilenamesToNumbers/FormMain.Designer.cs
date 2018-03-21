d35cdeacb165f92db869b98db9d8d2e0f11c6b0f
Morbidslinky <joe.h.zoeller@gmail.com>
2018-03-20 19:58:23 -0500

    centered application, locked size

--- FilenamesToNumbers/FormMain.Designer.cs
+++ FilenamesToNumbers/FormMain.Designer.cs
@@ -134,7 +134,6 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
             this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(253, 237);
             this.Controls.Add(this.labelExample);
@@ -144,9 +143,12 @@
             this.Controls.Add(this.numericUpDownBeginCount);
             this.Controls.Add(this.labelBeginCount);
             this.Controls.Add(this.buttonSelectFiles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "FormMain";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Rename Files to Numbers";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBeginCount)).EndInit();
             this.ResumeLayout(false);
